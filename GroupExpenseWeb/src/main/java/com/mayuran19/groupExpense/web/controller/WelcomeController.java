bc7a42caa7ac5ae60ff5f5c8fb7eea432b7d61af
mayuran19 <mayuran19@gmail.com>
2016-01-05 00:09:35 +0800

    Moved to java based spring configuration from xml based

--- GroupExpenseWeb/src/main/java/com/mayuran19/groupExpense/web/controller/WelcomeController.java
+++ GroupExpenseWeb/src/main/java/com/mayuran19/groupExpense/web/controller/WelcomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
     @RequestMapping(value = "/welcome/greet", method = RequestMethod.GET)
-    public String greet(){
+    public String greet() {
         return "Hello World";
     }
 }
